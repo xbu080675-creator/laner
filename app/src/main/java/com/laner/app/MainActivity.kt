e76686210d98883e2c5e94178fae5bbadc1bdc0b
PocketSpace <xbu080675@gmail.com>
2026-09-13 00:29:37 +0800

    feat: wire draft HUD preview entry

--- app/src/main/java/com/laner/app/MainActivity.kt
+++ app/src/main/java/com/laner/app/MainActivity.kt
@@ -65,6 +65,11 @@ class MainActivity : ComponentActivity() {
                             riftScreenRunning = true
                         }
                     },
+                    onStartDraftHudPreview = {
+                        if (riftScreenController.startDraftPreview()) {
+                            riftScreenRunning = true
+                        }
+                    },
                     onStopRiftScreen = {
                         riftScreenController.stop()
                         riftScreenRunning = false
