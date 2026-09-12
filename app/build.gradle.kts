cfb7fa18efafd8a4e9262d65c3100256c7ace36d
PocketSpace <xbu080675@gmail.com>
2026-09-12 19:48:51 +0800

    test: enable app persistence unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20240303")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
